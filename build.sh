bb5d506c3cae8d9161ab6575f4dddc38365ae977
Nikolay Chechulin <nchechulin@protonmail.com>
2022-03-12 12:24:30 +0300

    setup env for multilingual documents

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Create a directory for output files (don't fail if it exists)
+mkdir -p output
+
+# Build English version of CV
+pdflatex "\let\ENGLISH=1 \input" main.tex
+mv main.pdf output/cv.pdf
+# Build Russian version of CV
+pdflatex "\let\RUSSIAN=1 \input" main.tex
+mv main.pdf output/cv_ru.pdf
+# Clean LaTeX auxiliary files
+latexmk -c
